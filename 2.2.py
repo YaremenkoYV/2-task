390eed21fb58d4d2f924f660a01c875d3295da6e
Yaremenko Yurii <125539657+YaremenkoYV@users.noreply.github.com>
2023-04-28 13:10:17 +0300

    Update 2.2.py

--- 2.2.py
+++ 2.2.py
@@ -1,14 +1,13 @@
-x = int(input('Введіть число яке хочете помножити: '))
-y = int(input('Введіть число на яке хочете помножити: '))
+a = int(input('Введіть число яке хочете помножити: '))
+b = int(input('Введіть число на яке хочете помножити: '))
 
 
 def mult(x, y):
     if x == 1:
-        return y 
+        return y
     elif y==1:
         return x
     else:
         return y + mult(x - 1, y)
 
-
-print(mult(x, y))
+print("Результат множення: ", mult(a, b))
