a7424f073a52a67d72e45db16541bd25014ee72a
Yaremenko Yurii <125539657+YaremenkoYV@users.noreply.github.com>
2023-04-28 13:16:33 +0300

    Update 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -1,9 +1,12 @@
-x = int(input('Введіть число яке хочете поділити: '))
-y = int(input('Введіть число на яке хочете поділити: '))
+a = int(input('Введіть число яке хочете поділити: '))
+b = int(input('Введіть число на яке хочете поділити: '))
 
 
 def deli(x, y):
-    if y == 0:
+    if y == 1:
+        return x
+    elif y==0:
         return
-    return deli(x - y, y) if x >= y else x
-print(deli(x, y))
+    else:
+        return deli(x - y, y) if x >= y else x
+print("Результат ділення: ", deli(a, b))
