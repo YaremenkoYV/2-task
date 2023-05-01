eef5301b366d7bc1700bde527f878416a6dc84d9
Yaremenko Yurii <125539657+YaremenkoYV@users.noreply.github.com>
2023-05-01 18:03:07 +0300

    Update 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -5,7 +5,7 @@ b = int(input('Введіть число на яке хочете поділит
 def deli(x, y):
     if y == 1:
         return x
-    elif y==0:
+    elif y == 0:
         return print("Ділити на 0 не можна")
     else:
         return deli(x - y, y) if x >= y else x
